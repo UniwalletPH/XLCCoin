f7a71fa8f6bcf0250612d5480ee32bc6c1215903
Vincent Dagpin <vincent.dagpin@live.com>
2019-11-21 16:03:19 +0800

    Added console as node

--- XLCCoin.Node/Program.cs
+++ XLCCoin.Node/Program.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using XLCCoin.Application.Interfaces;
 using XLCCoin.Persistence;
@@ -8,20 +12,41 @@ namespace XLCCoin.Node
 {
     class Program
     {
+        static IServiceCollection _services;
+
+        static ServiceProvider ServiceProvider
+        {
+            get
+            {
+                if (_services == null) _services = new ServiceCollection();
+
+                var _builder = new ConfigurationBuilder()
+                   .SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json", optional: true);
+
+                var _config = _builder.Build();
+
+                _services.AddDbContext<XLCDbContext>(options =>
+                {
+                    options.UseSqlServer(_config.GetConnectionString("XLCConStr"));
+                })
+                .AddScoped<ISecurity, NetworkSecurity>()
+                .AddScoped<IXLCDbContext>(provider => provider.GetService<XLCDbContext>());
+
+                var _serviceProvider = _services.BuildServiceProvider();
+
+                return _serviceProvider;
+            }
+        }
+
         static void Main(string[] args)
         {
-            var _serviceProvider = new ServiceCollection()
-                .AddScoped<ISecurity, NetworkSecurity>()
-                .AddScoped<IXLCDbContext, XLCDbContext>()
-                .BuildServiceProvider();
+            IXLCDbContext _context = ServiceProvider.GetService<IXLCDbContext>();
 
+            var _w = _context.Nodes.ToList();
 
-            IXLCDbContext _context = _serviceProvider.GetService<IXLCDbContext>();
-
-
-
-
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Nodes: {0}", _w.Count);
+            Console.ReadLine();
         }
     }
 }
